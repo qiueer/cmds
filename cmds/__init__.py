c24e8ead63670729fcb72fafe1016ee9ca4aee0a
qiueer <qiujqin@163.com>
2016-03-11 15:00:16 +0800

    init

---
+++ cmds/__init__.py
@@ -0,0 +1,63 @@
+#-*- encoding:utf-8 -*-
+
+import re
+import types
+import platform
+import datetime
+import os
+import time
+import signal
+
+from subprocess import PIPE, Popen
+
+class cmd(object):
+    def __init__(self, *args, **kwargs):
+        self.stdout = None
+        self.stderr = None
+        self.retcode = 0
+        self.cmd(*args, **kwargs)
+        
+    def cmd(self, command, env=None, stdout=PIPE, stderr=PIPE, timeout=None):
+            
+            ps = None
+            if platform.system() == "Linux":
+                    ps = Popen(command, stdout=stdout, stderr=stderr, shell=True)
+            else:
+                    ps = Popen(command, stdout=stdout, stderr=stdout, shell=False)
+            
+            if timeout:
+                start = datetime.datetime.now()
+                while ps.poll() is None:
+                        print "check timeout,timeout:%s" %(timeout)
+                        time.sleep(0.2)
+                        now = datetime.datetime.now()
+                        if (now - start).seconds > timeout:
+                                os.kill(ps.pid, signal.SIGINT)
+                                self.retcode = -1
+                                self.stdout = None
+                                self.stderr = None
+                                return self
+
+            (self.stdout, self.stderr) =ps.communicate()
+            self.retcode = ps.returncode
+
+            return self
+
+    def __str__(self):
+        import json
+        res = {"stdout":self.stdout, "stderr": self.stderr, "retcode": self.retcode}
+        return  json.dumps(res, separators=(',', ':'), ensure_ascii=False).encode('utf-8') 
+    
+    def stdo(self):
+        if self.stdout:
+            return self.stdout.strip().decode(encoding='UTF-8')
+        return ''
+    
+    def stde(self):
+        if self.stderr:
+            return self.stderr.strip().decode(encoding='UTF-8')
+        return ''
+    
+    def code(self):
+        return self.retcode
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# # coding: utf-8
+from setuptools import find_packages, setup
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+setup(
+    name='cmds',
+    description='cmds for Python',
+    version='0.1',
+    long_description=long_description,
+    author='qiueer',
+    author_email='qiujqin@163.com',
+    url='https://github.com/qiueer/cmds',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Production/Stable',
+        'Intended Audience :: Developers',
+        ('License :: OSI Approved :: GNU Library or Lesser '
+         'General Public License (LGPL)'),
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
